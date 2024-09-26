60d17c2d907cb222f1fd65ffa373a7a676170f23
DENEME <mevahap@hotmail.com>
2024-09-27 01:54:00 +0300

    Initial commit

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,37 @@
+package Utility;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseDriver {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static JavascriptExecutor js;
+
+
+    //bunun şartı extends olması ve başta yer alması
+    //public static void BaslangicIslemleri() // tearStart()
+    static // tearStart()
+    {
+
+        driver=new ChromeDriver();
+
+        // driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
+        wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+        js=(JavascriptExecutor)driver;
+
+
+    }
+    public static void BekleKapat() // tearDown()
+    {
+        MyFunc.Bekle(3);
+        driver.quit();
+    }
+}
